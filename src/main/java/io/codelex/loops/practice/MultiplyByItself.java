0e53bfe50631d06dafe3be004a7c9f6e977837e8
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-05-10 19:41:49 +0300

    Loops 2 Task

--- src/main/java/io/codelex/loops/practice/MultiplyByItself.java
+++ src/main/java/io/codelex/loops/practice/MultiplyByItself.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 public class MultiplyByItself {
 
     public static void main(String[] args) {
-        int i, n;
-
+        int i;
+        int n;
+        System.out.println("Input number : ");
+        Scanner number = new Scanner(System.in);
+        i = number.nextInt();
         System.out.print("Input number of terms : ");
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
 
-        /*
-        todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.pow()
-        for (.......) {
-          System.out.println(........);
+
+
+        // todo - complete loop to multiply i with itself n times, it is NOT allowed to use Math.pow()
+        for (int j = 1; j < n; j++) {
+            i *= i;
+
         }
-        */
+        {
+            System.out.println(i);
+        }
+
     }
 
 }
