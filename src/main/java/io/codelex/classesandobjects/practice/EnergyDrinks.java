0f46ad44dc0c849a32308202211894ad82aee289
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-05-17 21:43:30 +0300

    Objects and Clases Exercise 6

--- src/main/java/io/codelex/classesandobjects/practice/EnergyDrinks.java
+++ src/main/java/io/codelex/classesandobjects/practice/EnergyDrinks.java
@@ -1,24 +1,26 @@
 package io.codelex.classesandobjects.practice;
 
-import io.codelex.NoCodeWrittenException;
-
 public class EnergyDrinks {
-    final int NUMBERED_SURVEYED = 12467;
-    final double PURCHASED_ENERGY_DRINKS = 0.14;
-    final double PREFER_CITRUS_DRINKS = 0.64;
+    final static int NUMBERED_SURVEYED = 12467;
+    final static double PURCHASED_ENERGY_DRINKS = 0.14;
+    final static double PREFER_CITRUS_DRINKS = 0.64;
+
 
     public static void main(String[] args) {
+
         //fixme
-        //System.out.println("Total number of people surveyed " + NUMBERED_SURVEYED);
-        //System.out.println("Approximately " + energyDrinkers + " bought at least one energy drink");
-        //System.out.println(preferCitrus + " of those " + "prefer citrus flavored energy drinks.");
+        System.out.println("Total number of people surveyed " + NUMBERED_SURVEYED);
+        System.out.println("Approximately " + EnergyDrinks.calculateEnergyDrinkers(NUMBERED_SURVEYED) + " bought at least one energy drink");
+        System.out.println(EnergyDrinks.calculatePreferCitris(NUMBERED_SURVEYED) + " of those " + "prefer citrus flavored energy drinks.");
     }
 
     public static double calculateEnergyDrinkers(int numberSurveyed) {
-        throw new NoCodeWrittenException();
+        return numberSurveyed * PURCHASED_ENERGY_DRINKS;
+
     }
 
     public static double calculatePreferCitris(int numberSurveyed) {
-        throw new NoCodeWrittenException();
+        return calculateEnergyDrinkers(numberSurveyed) * PREFER_CITRUS_DRINKS;
+
     }
 }
