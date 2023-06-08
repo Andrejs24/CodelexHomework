34377c7cb969b18b5825b6850b2386abd6c7c4c0
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-06-08 23:22:27 +0300

    Polymorphism Exercise 4

--- src/main/java/io/codelex/polymorphism/practice/exercise4/Staff.java
+++ src/main/java/io/codelex/polymorphism/practice/exercise4/Staff.java
@@ -7,7 +7,7 @@ public class Staff {
     // Sets up the list of staff members.
     //-----------------------------------------------------------------
     public Staff() {
-        staffList = new StaffMember[6];
+        staffList = new StaffMember[8];
         staffList[0] = new Executive("Sam", "123 Main Line",
                 "555-0469", "123-45-6789", 2423.07);
         staffList[1] = new Employee("Carla", "456 Off Line",
@@ -20,8 +20,17 @@ public class Staff {
                 "555-8374");
         staffList[5] = new Volunteer("Cliff", "321 Duds Lane",
                 "555-7282");
+        staffList[6] = new Commission("Andrew", "First street 45",
+                "432-66-987", "156789", 9.75, 15);
+        staffList[7] = new Commission("Barbara", "Second Street 54", "554-66-123",
+                "542135", 6.25, 20);
         ((Executive) staffList[0]).awardBonus(500.00);
         ((Hourly) staffList[3]).addHours(40);
+        ((Commission) staffList[6]).addScale(400.00);
+        ((Hourly) staffList[6]).addHours(35);
+        ((Commission) staffList[7]).addScale(950.00);
+        ((Hourly) staffList[7]).addHours(40);
+
     }
 
     //-----------------------------------------------------------------
