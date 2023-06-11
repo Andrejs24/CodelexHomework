e2c4d5680141607ec8b9f1a8588944e9238cf7ce
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-06-12 01:41:19 +0300

    Summary Generics Combiner

--- src/main/java/io/codelex/oop/summary/generics/Combiner.java
+++ src/main/java/io/codelex/oop/summary/generics/Combiner.java
@@ -1,13 +1,11 @@
 package io.codelex.oop.summary.generics;
 
-import java.math.BigDecimal;
-
 public class Combiner {
 
     //Fix this method so it can receive two any kind of objects
     //Test the functionality
 
-    public static String combineTwoItems(Integer first, BigDecimal second) {
+    public static <E, T> String combineTwoItems(E first, T second) {
         return "First item: " + first.toString() + "; Second item: " + second.toString();
     }
 
