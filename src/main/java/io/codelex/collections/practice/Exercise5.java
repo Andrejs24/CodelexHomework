e3e2d87845657261739fe9432a711a5abbfbb024
Andrejs24 <andrejs.kuramsins@gmail.com>
2023-06-15 12:37:11 +0300

    Exercise 5 Now works correctly

--- src/main/java/io/codelex/collections/practice/Exercise5.java
+++ src/main/java/io/codelex/collections/practice/Exercise5.java
@@ -1,5 +1,7 @@
 package io.codelex.collections.practice;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Exercise5 {
@@ -10,10 +12,33 @@ public class Exercise5 {
         boolean isHappy = true;
         char[] arrayOfInputs = (String.valueOf(input)).toCharArray();
         int result = 0;
-        for (char number : arrayOfInputs) {
-            result += (int) Math.pow((Character.getNumericValue(number)), 2);
+        List<Integer> listOfValuesAlreadyExist = new ArrayList<>();
+        while (result != 1 || result != input) {
 
+            for (char number : arrayOfInputs) {
+                result += (int) Math.pow((Character.getNumericValue(number)), 2);
+
+            }
+            if (result == 1) {
+                break;
+            } else if (result == input) {
+                isHappy = false;
+                break;
+            } else if (listOfValuesAlreadyExist.contains(result)) {
+                isHappy = false;
+                break;
+            } else
+                System.out.println(listOfValuesAlreadyExist.size());
+            listOfValuesAlreadyExist.add(listOfValuesAlreadyExist.size(), result);
+            System.out.println(listOfValuesAlreadyExist.size());
+            System.out.println(listOfValuesAlreadyExist);
+            arrayOfInputs = (String.valueOf(result)).toCharArray();
+            result = 0;
         }
-        System.out.println(result % 2 == 1 ? isHappy : !isHappy);
+
+
+        System.out.println(isHappy ? "Number is happy!" : "Number is not happy.");
     }
 }
+
+
